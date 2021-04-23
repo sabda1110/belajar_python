e2382b3650296b6ab9bbd86e4ce1f0e146cfaea2
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-23 14:04:14 +0100

    Belajara tipe data Python

--- index.py
+++ index.py
@@ -1,7 +1,37 @@
-a = 10
-b = 5
-c=10+5
+# Belajar Tipe Data
 
-print("Nilai A adalah ",a)
-print("Nilai B adalah ",b)
-print("Hasil penjumlahan dari a + b adalah = ",c)
+# tipe data Int
+nilai_int = 10;
+print(type(nilai_int))
+print("Nilai dari Integer = ",nilai_int)
+
+# tipe data Float 
+nilai_float = 1.15;
+print(type(nilai_float))
+print("Nilai dari Float = ",nilai_float)
+
+# tipe data String
+nilai_string = "Saya Belajar dengan Serius"
+print(type(nilai_string))
+print("Nilai dari String = ",nilai_string)
+
+#tipe data Boolean
+nilai_boolean =True;
+print(type(nilai_boolean))
+print("Nilai dari Boolean = ",nilai_boolean)
+
+#Tipe data Kompleks
+nilai_complex = complex(5,6)
+print(type(nilai_complex))
+print("Nilai dari Boolean = ",nilai_complex)
+
+#menggambil tipe data dari bahasa C
+from ctypes import c_double
+data_c = c_double(20.5)
+print(type(data_c))
+print("Nilai dari Boolean = ",data_c)
+
+
+
+
+
