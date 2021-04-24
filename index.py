ba5fb67b32919620fab71b3211792cdcd5d369c5
sabda1110 <sabdasetiawan205@gmail.com>
2021-04-24 13:36:42 +0100

    Belajar Inputan dari User

--- index.py
+++ index.py
@@ -1,51 +1,17 @@
-# Belajar Cesting ,Cesting Merupakan mengubah tipe data
+# Belajar mengambil data dari user
 
-print("============INTEGER=================")
+#string
+data = input("Masukan Data Anda : ")
+print("Nilai : ",data," , Type = ",type(data))
 
-data_int = 10;
-print("data : ",data_int,", Type = ",type(data_int))
+#int
+data_int = int(input("Masukan Data Anda : "))
+print("Nilai : ",data_int," , Type = ",type(data_int))
 
-data_float=float(data_int)
-data_string=str(data_int)
-data_bool=bool(data_int) #benilai FALSE jika nilai tersebut 0 dan selai 0 maka TRUE
-print("data : ",data_int,", Type = ",type(data_float))
-print("data : ",data_int,", Type = ",type(data_string))
-print("data : ",data_int,", Type = ",type(data_bool))
+#Float
+data_float = float(input("Masukan Data Anda : "))
+print("Nilai : ",data_float," , Type = ",type(data_float))
 
-print("============FLOAT=================")
-
-data_float = 10.9;
-print("data : ",data_float,", Type = ",type(data_float))
-
-data_int=int(data_float)
-data_string=str(data_float)
-data_bool=bool(data_float)
-print("data : ",data_int,", Type = ",type(data_int))
-print("data : ",data_string,", Type = ",type(data_string))
-print("data : ",data_bool,", Type = ",type(data_bool))
-
-
-print("============BOOLEAN=================")
-
-data_bool = True;
-print("data : ",data_bool,", Type = ",type(data_bool))
-
-data_int=int(data_bool)
-data_string=str(data_bool)
-data_float=float(data_bool)
-print("data : ",data_int,", Type = ",type(data_int))
-print("data : ",data_string,", Type = ",type(data_string))
-print("data : ",data_float,", Type = ",type(data_float))
-
-
-print("============STRING=================")
-
-data_string = "10";
-print("data : ",data_string,", Type = ",type(data_string))
-
-data_int=int(data_string) #string harus angka
-data_bool=bool(data_string) #string harus angka
-data_float=float(data_string) #nilai string bila terisi bernilai true dan bila tak terisi maka false
-print("data : ",data_int,", Type = ",type(data_int))
-print("data : ",data_bool,", Type = ",type(data_bool))
-print("data : ",data_float,", Type = ",type(data_float))
+#boolean
+data_bool = bool(int(input("Masukan Data Bolean : ")))
+print("Nilai : ",data_bool," , Type = ",type(data_bool))
